6000426867efe1acd1eca3aa71425eff5c6c1400
Dutt <anshuk.dutt@capgemini.com>
2022-10-10 06:28:00 +0530

    New Simple commit

---
+++ src/main/java/io/github/anshdutt/csvdemo/TestMe.java
@@ -0,0 +1,16 @@
+package io.github.anshdutt.csvdemo;
+
+public class TestMe {
+    public static  void yaayMahima(String name){
+        System.out.println("yaayMahima");
+    }
+    public static  void yaayStephy(String name){
+        System.out.println("yaayStephy");
+    }
+    public static  void yaayDheeraj(String name){
+        System.out.println("yaayStephy");
+    }
+    public static  String yaayTejaswi(String name){
+      return "yaay Tejaswi";
+    }
+}
---
+++ src/test/java/io/github/anshdutt/csvdemo/CsvdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.anshdutt.csvdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
